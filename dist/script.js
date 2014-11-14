65dd65c9c3a8406a710f3223aa5a4b7baeb3beca
pcsandford <petersandford1982@gmail.com>
2014-11-13 21:53:23 -0500

    added GSAP to the project to control timing of the curtains, title and logo. started to rework the javascript and jquery accordingly so that slideshow matches up

--- dist/script.js
+++ dist/script.js
@@ -1,46 +1,106 @@
-
-  $(document).ready(function(){
+$(document).ready(function(){
    console.log('ready');
 
-  //fade in of main text
-  $('.headerText').fadeIn( 3000 );
+var rightCover = $('.rightCover'),
+    leftCover = $('.leftCover'),
+    headerText = $('.headerText'),
+    logoAreaMain = $('.logoAreaMain')
+    tl = new TimelineMax();
 
-  //automatic opening of cover
- var openingTimer = setTimeout(autoOpen, 5000);
 
- 
+//setting up the opening timeline for the animation
 
-  //open curtain on click event
-  $('html').on('click',function(){
-    $('.leftCover').css("left","-50vw");
-    $('.rightCover').css("right","-50vw");
-    $('.logoAreaMain').fadeIn().css("opacity","1");
-    $('.headerText').fadeOut().css("opacity","0");
+var openTimeline = new TimelineMax();
+function getOpenAnimation(){
+      openTimeline.to(headerText,3,{opacity:'1'});
+      openTimeline.to(headerText,0,{opacity:'0'});
+      openTimeline.to(rightCover,0, {right:'-50vw'});
+      openTimeline.to(leftCover,0, {left:'-50vw'});
+      openTimeline.to(logoAreaMain,0,{opacity:'1'})
+      openTimeline.addLabel('openCompleted');
+
+    return openTimeline;
+  }
+
+//add the items to the opening timeline
+    
+  tl.add('headerText')
+    .add('leftCover')
+    .add('rightCover')
+    .add('logoAreaMain')
+
+//the function that plays the opening animation
+function playOpenAnimation(){
+    console.log("open");
+    tl.play(getOpenAnimation());
+    numberofSlides = 0;
+    automaticSliderTimer = setInterval(slideSwitch, 10000 );
+   };
+playOpenAnimation();
+
+//auto open on touch
+$('html').on('click',function(){
+    console.log('jump to open and pause');
+    $('html').unbind('click');
+    openTimeline.pause(3);
   });
+
+//setting up the closing timeline animaition
+ 
+var tl2 = new TimelineMax();
+
+var closeTimeline = new TimelineMax();
+function getCloseAnimation(){
+      closeTimeline.to(rightCover,0, {right:'0vw'});
+      closeTimeline.to(leftCover,0, {left:'0vw'});
+      closeTimeline.to(logoAreaMain,0,{opacity:'0'})
+      closeTimeline.addLabel('openCompleted');
+
+    return closeTimeline;
+  }
+
+
+//add the items to the opening timeline
+    
+  tl2.add('leftCover')
+     .add('rightCover')
+     .add('logoAreaMain')
+
+
+     //the function that plays the opening animation
+     function playCloseAnimation(){
+         console.log("open");
+         tl2.play(getCloseAnimation());
+        };
+     playCloseAnimation();
+
+  var closingTimer = setTimeout(playCloseAnimation, 40000)   
+  //open curtain on click event
+  // $('html').on('click',function(){
+  //   $('.leftCover').css("left","-50vw");
+  //   $('.rightCover').css("right","-50vw");
+  //   $('.logoAreaMain').fadeIn().css("opacity","1");
+  //   $('.headerText').fadeOut().css("opacity","0");
+  // });
 
   //automatic closing of cover and reset of cycle
 
  // var closingTimer = setTimeout(autoClose, 10000);
 
-  function autoClose(){
-    openingTimer = setTimeout(autoOpen, 5000);
-    $('.leftCover').css("left","0vw");
-    $('.rightCover').css("right","0vw");
-    $('.logoAreaMain').fadeOut().css("opacity","0");
-    $('.headerText').fadeIn( 3000 );
-    clearTimeout(closingTimer);
+ //  function autoClose(){
+ //    $('.leftCover').css("left","0vw");
+ //    $('.rightCover').css("right","0vw");
+ //    $('.logoAreaMain').fadeOut().css("opacity","0");
+ //    $('.headerText').fadeIn( 3000 );
+ //  };
 
-  };
-
- function autoOpen(){
-    $('.leftCover').css("left","-50vw");
-    $('.rightCover').css("right","-50vw");
-    $('.logoAreaMain').fadeIn().css("opacity","1");
-    $('.headerText').fadeOut().css("opacity","0");
-    clearTimeout(openingTimer);
-    numberofSlides = 0;
-    automaticSliderTimer = setInterval(slideSwitch, 10000 );
-  };
+ // function autoOpen(){
+ //    $('.leftCover').css("left","-50vw");
+ //    $('.rightCover').css("right","-50vw");
+ //    $('.logoAreaMain').fadeIn().css("opacity","1");
+ //    $('.headerText').fadeOut().css("opacity","0");
+    
+ //  };
 
 
 
@@ -111,12 +171,7 @@ var numberofSlides = 0
       $('.active').fadeIn();
       }else{
         clearTimeout(automaticSliderTimer);
-        autoClose();
 
       };
     };
  });//close main function
-
-
-
-        
